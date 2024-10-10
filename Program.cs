07b181de39bce397f706e16b94b32dd6af29a242
Yaaseen Khan <yaaseen.khan@redpandasoftware.co.za>
2024-10-10 16:23:39 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Write("Enter a string:");
+string input = Console.ReadLine();
+
+input = input.ToLower();
+
+int left = 0;
+int right = input.Length - 1;
+bool isPalendrome = true;
+while(left < right)
+{
+    if(input[left] != input[right])
+    {
+        isPalendrome = false;
+        break;
+    }
+    left++;
+    right--;
+}
+if (isPalendrome)
+{
+    Console.WriteLine("the string is a palindrome");
+}
+else
+{
+    Console.WriteLine("the string is not a palendrome");
+}
+
+Console.ReadLine();
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Technical-Task-string-is-a-palindrome" 
